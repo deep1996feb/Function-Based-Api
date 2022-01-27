0ba026d2526bc9b6178fb4c9d94ebf77f6363b48
Deepanshu <deepsoni871@gmail.com>
2022-01-27 10:15:28 +0530

    Initial commit

---
+++ myapp.py
@@ -0,0 +1,58 @@
+from wsgiref import headers
+from django.http import request
+import requests
+import json
+
+URL = "http://127.0.0.1:8000/student_api/"
+
+def get_data(id = None):
+    data = {}
+    if id is not None:
+        data = {'id': id}
+    json_data = json.dumps(data)
+    headers = {'content-Type': 'application/json'}
+    r = requests.get(url=URL, headers = headers, data = json_data)
+    data = r.json()
+    print(data)
+#get_data()
+
+#Post data
+def post_data():
+ data = {
+  'name': 'Shikhar Dhawan',
+  'roll': 39,
+  'address': 'Delhi'
+ }
+ headers = {'content-Type': 'application/json'}
+ json_data = json.dumps(data)
+ r = requests.post(url=URL,headers=headers, data = json_data)
+ data = r.json()
+ print(data)
+#post_data()
+
+def update_data():
+ data = {
+  'id': 4,
+  'name': 'Shikhi',
+  'roll': '35'
+ }
+ headers = {'content-Type':'application/json'}
+
+ json_data = json.dumps(data)
+ r = request.put(url=URL, headers=headers, data = json_data)
+ data = r.json()   
+ print(data)
+#update_data()
+
+def delete_data():
+    data = {'id': 2}
+    headers = {'content-Type':'application/json'}
+    json_data = json.dumps(data)
+    r = requests.delete(url=URL, headers=headers, data=json_data)
+    data = r.json()
+    print(data)
+delete_data()
+
+
+
+
